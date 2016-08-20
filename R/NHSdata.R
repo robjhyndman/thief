3c33ef072b4aebe95c9db7fdb2339aa43c303e06
robjhyndman <robjhyndman@gmail.com>
2016-08-20 11:58:59 +1000

    Added A&E demand data

---
+++ R/NHSdata.R
@@ -0,0 +1,21 @@
+#' Accident and Emergency demand in the UK
+#'
+#' Weekly demand of Accident & Emergency departments in the UK, 
+#' from 7 November 2010 to 7 June 2015.
+#'
+#' @docType data
+#'
+#' @usage AEdemand
+#'
+#' @format An object of class \code{ts}.
+#'
+#' @keywords datasets
+#'
+#' @references 
+#'
+#' @examples
+#' library(ggplot2)
+#' autoplot(AEdemand, xlab="Year", ylab="Demand ('000)") +
+#'   ggtitle("Accident & Emergency Demand in the UK")
+"AEdemand"
+
